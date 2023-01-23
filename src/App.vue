3493dfa23d1b58d70f780eea00755fb96f7ac244
habibi963 <107906015+habibi963@users.noreply.github.com>
2023-01-24 00:13:04 +0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  data(){
+    return {
+      datasensor: "60",
+      datasensor2: "80",
+    };
+  },
+  created() {
+    //this.getData();
+  },
+  methods: {
+    async getData() {
+      const data = await fetch("http://localhost:83/sensor1")
+      const datajson = await data.json()
+      this.datasensor = datajson.sensor1
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    hello {{ datasensor }}
+    <button @click="getData" class="bg-orange-400 p-5">Ambil data</button>
+  </div>
+</template>
+
